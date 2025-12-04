890fbc2fc683f86485e25769cb871bb5837007f9
Kale Burns <157818175+KERUITSICEDARVILLE@users.noreply.github.com>
2025-12-03 20:18:31 -0500

    Added menu back in with new main changes

--- Assets/Scripts/Next_Scene_Start_Button.cs
+++ Assets/Scripts/Next_Scene_Start_Button.cs
@@ -7,34 +7,9 @@ using UnityEngine.UIElements;
 public class Next_Scene_Start_Button : MonoBehaviour
 {
     // This method is called when the user clicks on the collider attached to this GameObject
-    private void OnMouseDown()
+    public void OnButtonClick()
     {
         SceneManager.LoadScene("GameScreen");
-    }
-
-    private void Update()
-    {
- 
-    } 
-
-    private void OnMouseOver()
-        {
-            transform.localPosition = new Vector3(0f, 0f, -(float)((Math.Sin(Time.timeAsDouble) + 1) * 3.0));
-        }
-
-    private void OnMouseEnter() 
-    {
-        // Change the cursor to a hand when hovering over the button
-        //transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(2.0f, 2.0f, 2.0f), Time.deltaTime * 100);
-        //transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-    }
-
-     private void OnMouseExit()
-    {
-        // Reset the scale back to normal when the mouse is no longer over the button
-        //transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(0.5f, 0.5f, 0.5f), Time.deltaTime * 100);
-
-    //transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
     }
 
 }
