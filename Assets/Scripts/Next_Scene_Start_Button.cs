70cee1d1fe67548e371f0ea98001e3892515742b
mlouden <mlouden@cedarville.edu>
2025-12-03 21:18:12 -0500

    Integration Final

--- Assets/Scripts/Next_Scene_Start_Button.cs
+++ Assets/Scripts/Next_Scene_Start_Button.cs
@@ -1,23 +1,36 @@
+using TMPro;
+using UnityEngine.UI;
+
 using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UIElements;
+//using UnityEngine.UIElements;
 
 public class Next_Scene_Start_Button : MonoBehaviour
 {
-    // This method is called when the user clicks on the collider attached to this GameObject
-IEnumerator WaitAndDoSomething(float secondsToWait)
-        {
-            yield return new WaitForSeconds(secondsToWait); // This line pauses execution
+    public Image Block;
+    public float TimerMax;
+    private float Timer;
 
-            SceneManager.LoadScene("GameScreen");
+    void Start() {
+        Timer = 0;
+    }
+    // This method is called when the user clicks on the collider attached to this GameObject
+    IEnumerator WaitAndDoSomething(float secondsToWait)
+    {
+        if (Timer < TimerMax) {
+        Timer += Time.deltaTime;
+        Block.color = new Color(1f, 1f, 1f, Timer/TimerMax);
         }
+        yield return new WaitForSeconds(secondsToWait); // This line pauses execution
+        SceneManager.LoadScene("GameScreen");
+    }
 
 
     public void OnButtonClick()
     {
-        StartCoroutine(WaitAndDoSomething(0.5f));
+        StartCoroutine(WaitAndDoSomething(TimerMax));
     }
 
 }
