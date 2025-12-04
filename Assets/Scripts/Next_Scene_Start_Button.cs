0eff90605056cbf2a825225e61b054dd6ba0af56
Kale Burns <157818175+KERUITSICEDARVILLE@users.noreply.github.com>
2025-12-03 21:51:29 -0500

    The Potential Final

--- Assets/Scripts/Next_Scene_Start_Button.cs
+++ Assets/Scripts/Next_Scene_Start_Button.cs
@@ -2,22 +2,53 @@ using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UIElements;
+//using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class Next_Scene_Start_Button : MonoBehaviour
 {
-    // This method is called when the user clicks on the collider attached to this GameObject
-IEnumerator WaitAndDoSomething(float secondsToWait)
-        {
-            yield return new WaitForSeconds(secondsToWait); // This line pauses execution
 
-            SceneManager.LoadScene("GameScreen");
+    public GameObject MaskingBlocker;
+    public float timeMax;
+    private float timer;
+
+    public bool isClicked = false;
+
+    // This method is called when the user clicks on the collider attached to this GameObject
+    /*IEnumerator WaitAndDoSomething(float secondsToWait)
+            {
+                yield return new WaitForSeconds(secondsToWait); // This line pauses execution
+
+                SceneManager.LoadScene("GameScreen");
+            }
+
+    */
+
+    void Update()
+    {
+        if (!isClicked) 
+        {
+            return;
         }
 
+        if (timer < timeMax)
+        {
+            timer += Time.deltaTime;
+            
+            MaskingBlocker.GetComponent<Image>().color = new Color(1f, 1f, 1f, timer / timeMax);
+            return;
+        }
 
+        SceneManager.LoadScene("GameScreen");
+    }
     public void OnButtonClick()
     {
-        StartCoroutine(WaitAndDoSomething(0.5f));
+        //StartCoroutine(WaitAndDoSomething(0.5f));
+
+        isClicked = true;
+
+        timer = 0f;
+        MaskingBlocker.SetActive(true);
     }
 
 }
